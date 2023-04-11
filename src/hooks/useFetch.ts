412891bd04c1779a002797c49b6cb8464e97a5d2
Maria Gryn <maria.grin2003@gmail.com>
2023-04-11 18:55:54 +0300

    created useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,26 @@
+import {useEffect, useState} from "react";
+
+interface State<T> {
+    data?: T,
+    error?: Error | null,
+    loading: boolean
+}
+
+function useFetch <T>(url: string): State<T> {
+    const [data, setData] = useState<T>();
+    const [error, setError] = useState<Error | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(url)
+            .then(res => res.json())
+            .then(res => setData(res))
+            .catch(err => setError(err))
+            .finally(() => setLoading(false))
+    }, [url])
+
+    return {data, error, loading}
+}
+
+export default useFetch;
